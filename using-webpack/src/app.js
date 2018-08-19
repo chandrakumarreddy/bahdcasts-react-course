a11914ba74946b74955b3e957c74d2a253e075b6
chandrakumarreddy <chandrapenugonda655@gmail.com>
2018-08-19 23:32:49 +0530

    react crud

---
+++ using-webpack/src/app.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+const App = () => <h1>Hello react</h1>;
+
+render(<App />, document.getElementById("root"));
+
+export default App;
